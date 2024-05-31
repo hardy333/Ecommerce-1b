f8a0a637d7395545c91df8f007c732da1323eda7
gela_samsonidze <samsonidze7@yahoo.com>
2024-05-31 21:45:35 +0400

    project setup, data fetching

---
+++ src/App.tsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+// import { Link } from "react-router-dom";
+// import data from "./data/data.json";
+// import Img1 from "./assets/product-xx99-mark-one-headphones/desktop/image-category-page-preview.jpg";
+
+export interface Product {
+  id: number;
+  slug: string;
+  name: string;
+  image: CategoryImage;
+  category: string;
+  categoryImage: CategoryImage;
+  new: boolean;
+  price: number;
+  description: string;
+  features: string;
+  includes: Include[];
+  gallery: Gallery;
+  others: Other[];
+}
+
+export interface CategoryImage {
+  mobile: string;
+  tablet: string;
+  desktop: string;
+}
+
+export interface Gallery {
+  first: CategoryImage;
+  second: CategoryImage;
+  third: CategoryImage;
+}
+
+export interface Include {
+  quantity: number;
+  item: string;
+}
+
+export interface Other {
+  slug: string;
+  name: string;
+  image: CategoryImage;
+}
+
+function App() {
+  const [prods, setProds] = useState<null | Product[]>(null);
+
+  const getData = async () => {
+    const res = await fetch("http://localhost:5173/data.json");
+    const data = await res.json();
+    setProds(data);
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      <h2 className="heading">Hello</h2>
+
+      {/* <img src={Img1} alt="" /> */}
+
+      {prods
+        ?.filter((prod) => prod?.category === "headphones")
+        .map((prod) => {
+          console.log(prod.categoryImage.desktop);
+
+          return (
+            <div key={prod.id}>
+              <p>{prod.new}</p>
+              <img
+                src={`http://localhost:5173/${prod.categoryImage.desktop}`}
+                alt=""
+              />
+            </div>
+          );
+        })}
+    </>
+  );
+}
+
+export default App;
